9faf45aca4566cab2cdd0934abf987903d468e4e
Kirill Davidenko <stafort777@gmail.com>
2023-02-26 23:53:07 +0300

    2 Task draft

--- task.js
+++ task.js
@@ -6,18 +6,15 @@ class AlarmClock {
   addClock(time, callback) {
     if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
-    } else if ( !(this.alarmCollection.find(item => item.time === time)) ) {
-      this.alarmCollection.push({time, callback, canCall: true});
-    } else {
+    } else if ( this.alarmCollection.find(setup => setup.time === time) ) {
       console.warn('Уже присутствует звонок на это же время');
     }
+    this.alarmCollection.push({time, callback, canCall: true});
   }
   removeClock(time) {
-    this.alarmCollection.forEach( (item, index) => {
-      if (item.time === time) {
-        this.alarmCollection.splice(index, 1);
-      }
-    })
+    this.alarmCollection = this.alarmCollection.filter(
+      setup => setup.time !== time
+    )
   }
   getCurrentFormattedTime() {
     return new Date().toLocaleTimeString().slice(0, -3);
@@ -25,22 +22,22 @@ class AlarmClock {
   start() {
     if (this.intervalId) {
       return
-    } else {
-      let startAlarmCollection = this.alarmCollection.forEach( item => {
-        if (item.time === this.getCurrentFormattedTime() && item.canCall) {
-          item.canCall = false;
-          item.callback();          
+    }
+    this.intervalId = setInterval( () => {
+      this.alarmCollection.forEach( setup => {
+        if (setup.time === this.getCurrentFormattedTime() && setup.canCall) {
+          setup.canCall = false;
+          setup.callback();          
         }
       })
-      this.intervalId = setInterval(startAlarmCollection, 1000)
-    }
+    }, 1000);
   }
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
   resetAllCalls() {
-    this.alarmCollection.forEach( item => item.canCall = true);
+    this.alarmCollection.forEach( setup => setup.canCall = true);
   }
   clearAlarms() {
     this.stop();
